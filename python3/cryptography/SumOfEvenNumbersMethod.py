00fe9116fb49704840248e4e9c4a44ad4f390ba0
GangGreenTemperTatum <104169244+GangGreenTemperTatum@users.noreply.github.com>
2023-02-03 21:46:28 -0800

    Create SumOfEvenNumbersMethod.py

---
+++ python3/cryptography/SumOfEvenNumbersMethod.py
@@ -0,0 +1,12 @@
+def sum_of_even_numbers(maximum):
+    total = 0
+
+    for number in range(1, maximum+1):
+        if(number % 2 == 0):
+            print("{0}".format(number))
+            total = total + number
+
+    print("The Sum of Even Numbers from 1 to {0} = {1}".format(number, total))
+
+maximum = int(input("Please Enter the Maximum Value : "))
+sum_of_even_numbers(maximum)
